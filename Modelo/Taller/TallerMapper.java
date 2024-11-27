19c9646598a00541a9362c1af73d8a1e7ba2cba4
ChristianPC <crisp314528@gmail.com>
2024-11-27 15:57:26 -0600

    Mapper Taller
    se crea el mapper del taller además de el package util

---
+++ Modelo/Taller/TallerMapper.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.Taller;
+
+import Model.Mapper.Mapper;
+import Utils.UtilDate;
+
+/**
+ *
+ * @author Student
+ */
+public class TallerMapper implements Mapper<Taller, TallerDTO>{
+
+    
+    @Override
+    public TallerDTO toDTO(Taller ent) {
+        return new TallerDTO(
+                ent.getId(),
+                ent.getNombre(),
+                UtilDate.toSqlDate(ent.getFecha()),
+                ent.getLugar(),
+                ent.getCapacidad());
+    }
+
+    @Override
+    public Taller toEnt(TallerDTO dto) {
+        return new Taller(
+                dto.getId(),
+                dto.getNombre(),
+                UtilDate.toLocalDate(dto.getFecha()),
+                dto.getLugar(),
+                dto.getCapacidad());
+    }
+    
+}
