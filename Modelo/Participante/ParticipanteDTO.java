e59acb9dc5a7c2508100cf0bd88389dd70ab9d48
ChristianPC <crisp314528@gmail.com>
2024-11-27 14:49:21 -0600

    Clase DTO
    Ajuste en la clase DTO

---
+++ Modelo/Participante/ParticipanteDTO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.Participante;
+
+/**
+ *
+ * @author Student
+ */
+public class ParticipanteDTO {
+    private final int cedula;
+    private final String nombre;
+    private final int telefono;
+    private final String correo;
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public ParticipanteDTO(int cedula, String nombre, int telefono, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+    
+    
+    
+}
